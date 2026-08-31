826600263c0e0d702edba5213e446a6a96df44c8
Yiqing Zhou <zhouyiqing@yadea.com>
2026-08-31 09:29:56 +0800

    Revert "style: QuickInputBox宽度从240px加到300px，发送按钮不再被截断"
    This reverts commit 5e101c15457cb8556a8e411cf865867cdc53adc9.

--- src/components/QuickInputBox.vue
+++ src/components/QuickInputBox.vue
@@ -431,7 +431,7 @@ onUnmounted(() => {
 
 /* 输入框容器 — 融入胶囊，无独立背景 */
 .input-box {
-  width: 300px;
+  width: 240px;
   border-radius: 0;
   background: transparent;
   display: flex;
@@ -705,7 +705,7 @@ onUnmounted(() => {
   }
   to {
     opacity: 1;
-    width: 300px;
+    width: 240px;
     transform: translateX(0);
   }
 }
